7e7e77bd5f879525b4b39e00ffc6df6a4f4c7ef1
Glenn Hiemstra <glenn.hiemstra@gmail.com>
2022-02-26 13:16:47 +0100

    first

---
+++ R/DutchWeather.R
@@ -0,0 +1,12 @@
+#' DutchWeather: A package for importing KKNMI weather data.
+#'
+#' The DutchWeather package provides functions to import weather statistics
+#' from the KNMI website.
+#'
+#' @section KNMI daily weather data:
+#' \code{\link[DutchWeather]{get_daily_zipdata}} \cr
+#'
+#' @docType package
+#' @name DutchWeather
+NULL
+#> NULL
