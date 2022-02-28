affcf5c06c47d5426ded004cbdf79d827d641765
Glenn Hiemstra <glenn.hiemstra@gmail.com>
2022-02-28 17:58:58 +0100

    add fun to extract data from weerlive.nl API

---
+++ R/get_Weerlive_API_data.R
@@ -0,0 +1,32 @@
+#' Get real time data from the Weerlive.nl API
+#'
+#' Don't forget to get your API acces key by visiting http://weerlive.nl/.
+#'
+#' @param key Int. Your API key
+#' @param locatie Character. Your location written in dutch. Default: "Amsterdam"
+#' @param base Character.
+#'
+#' @return
+#' @export
+#' #get_Weerlive_API_data(key="######", locatie="Amsterdam")
+get_Weerlive_API_data <- function(
+  key,
+  locatie="Amsterdam",
+  base="https://weerlive.nl/api/json-data-10min.php?key=%1.0f&locatie=%s"
+){
+  # control correct input
+  stopifnot(is.numeric(key) && is.character(locatie))
+
+  # create url to acces the WeerLive.nl API
+  url <- sprintf(base, key, locatie)
+
+  # acces API and transform data
+  data <- httr::GET(url)
+  raw_content <- base::rawToChar(data$content)
+
+  # return as tibble dataframe
+  df <- tibble::as_tibble(
+    jsonlite::fromJSON(raw_content)$liveweer
+  )
+  return(df)
+}
